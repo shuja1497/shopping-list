4e6cba046c769c12acc905fd7177b1d1543e909f
shuja1497 <shujareshi1497@gmail.com>
2026-02-09 20:25:56 +0530

    Add ShoppingListUiEvent sealed interface for UI event handling

---
+++ Shopping/app/src/main/java/com/droid/shopping/presentation/ShoppingListUiEvent.kt
@@ -0,0 +1,32 @@
+package com.droid.shopping.presentation
+
+import com.droid.shopping.data.local.Category
+import com.droid.shopping.data.local.ShoppingItem
+
+sealed interface ShoppingListUiEvent {
+    data class OnItemNameChanged(val name: String) : ShoppingListUiEvent
+
+    data class OnCategorySelected(val category: Category) : ShoppingListUiEvent
+
+    data object OnAddItemClicked : ShoppingListUiEvent
+
+    data class OnTogglePurchased(val item: ShoppingItem) : ShoppingListUiEvent
+
+    data class OnDeleteItem(val item: ShoppingItem) : ShoppingListUiEvent
+
+    data class OnFilterChanged(val category: Category?) : ShoppingListUiEvent
+
+    data class OnSortChanged(val sortOption: SortOption) : ShoppingListUiEvent
+
+    data class OnEditItem(val item: ShoppingItem) : ShoppingListUiEvent
+
+    data class OnEditNameChanged(val name: String) : ShoppingListUiEvent
+
+    data class OnEditCategoryChanged(val category: Category) : ShoppingListUiEvent
+
+    data object OnEditConfirmed : ShoppingListUiEvent
+
+    data object OnEditDismissed : ShoppingListUiEvent
+
+    data object OnErrorDismissed : ShoppingListUiEvent
+}
