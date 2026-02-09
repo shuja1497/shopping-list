8e0ff2707d63f679cae98764d708167da517d46f
shuja1497 <shujareshi1497@gmail.com>
2026-02-09 20:02:52 +0530

    Add ShoppingItem model

---
+++ Shopping/app/src/main/java/com/droid/shopping/data/local/ShoppingItem.kt
@@ -0,0 +1,13 @@
+package com.droid.shopping.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "shopping_items")
+data class ShoppingItem(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val name: String,
+    val category: Category,
+    val isPurchased: Boolean = false,
+)
